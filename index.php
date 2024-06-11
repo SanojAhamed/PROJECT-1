a4b0587ff133ee8d506486308e89b9985e0d3747
Sanoj Ahamed <sanojahamed1730@gmail.com>
2024-06-11 10:53:59 +0530

    fixed bug

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 <!DOCTYPE html>
 <html>
 
-
 <head>
 <link rel="shortcut icon" type="png" href="http://localhost/Quizzify/Quizzify/images/icon2.png">
 	<meta charset="utf-8">
@@ -11,9 +10,9 @@
 	<title>Quizzify</title>
 	<meta name="desciption" content="">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" type="text/css" href="../Interface/css_files/Home.css">
-	<link rel="stylesheet" type="text/css" href="../Interface/css_files/style.css">
-	<link rel="stylesheet" type="text/css" href="../Chatbot/style.css">
+	<link rel="stylesheet" type="text/css" href="Interface/css_files/Home.css">
+	<link rel="stylesheet" type="text/css" href="Interface/css_files/style.css">
+	<link rel="stylesheet" type="text/css" href="Chatbot/style.css">
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,1,0" />
     <link  rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,1,0"/>
@@ -36,30 +35,30 @@
 	<!-- Navigation Bar -->
 	<header id="header">
 		<nav>
-			<div class="logo"><img src="../images/QUIZZIFY.png" alt="logo"></div>
+			<div class="logo"><img src="images/QUIZZIFY.png" alt="logo"></div>
 			<ul>
-				<li><a class="active" href="../Interface/Home.php">Home</a></li>
-				<li><a href="../QuizCreationapi/index.php">Quiz</a></li>
-				<li><a href="../blog/index.php">Blog</a></li>
+				<li><a class="active" href="Home.php">Home</a></li>
+				<li><a href="QuizCreationapi/index.php">Quiz</a></li>
+				<li><a href="blog/index.php">Blog</a></li>
 				<li><a href="#about_section">About us</a></li>
 				<!-- <li><a href="../Interface/contactus.php">Contact Us</a></li> -->
-				<li><a href="../Interface/team.php">Team</a></li>
+				<li><a href="Interface/team.php">Team</a></li>
 				<?php
                     if (isset($_SESSION["id"])) {
                         if ($_SESSION["user_type"] === "dual") {
 
                     ?>
-                            <li><a href="../Interface/chooseRole.php">Create/Play</a></li>
+                            <li><a href="Interface/chooseRole.php">Create/Play</a></li>
                         <?php
                         } elseif ($_SESSION["user_type"] === "participant") {
 
                         ?>
-                            <li><a href="../Interface/chooseRole.php">Play</a></li>
+                            <li><a href="Interface/chooseRole.php">Play</a></li>
                         <?php
                         } elseif ($_SESSION["user_type"] === "instructor") {
 
                         ?>
-                            <li><a href="../Interface/chooseRole.php">Create</a></li>
+                            <li><a href="Interface/chooseRole.php">Create</a></li>
                     <?php
                         }
                     }
@@ -72,70 +71,70 @@
                 if ($_SESSION["user_type"] === "dual") {
 
             ?>
-                    <a href="../Profile/user_profile.php" class="profile"><?php echo $_SESSION["username"]; ?></a>
+                    <a href="Profile/user_profile.php" class="profile"><?php echo $_SESSION["username"]; ?></a>
                 <?php
                 } elseif ($_SESSION["user_type"] === "participant") {
 
                 ?>
-                    <a href="../Includes/logout.inc.php" class="profile"><?php echo $_SESSION["username"]; ?></a>
+                    <a href="Includes/logout.inc.php" class="profile"><?php echo $_SESSION["username"]; ?></a>
                 <?php
                 } elseif ($_SESSION["user_type"] === "instructor") {
 
                 ?>
-                    <a href="../Includes/logout.inc.php" class="profile"><?php echo $_SESSION["username"]; ?></a>
+                    <a href="Includes/logout.inc.php" class="profile"><?php echo $_SESSION["username"]; ?></a>
                 <?php
                 }
             } else {
 
                 ?>
-                <a class="get-started" href="../Interface/Register.php">Get Started</a>
+                <a class="get-started" href="Interface/Register.php">Get Started</a>
             <?php
             }
             ?>
-			<img src="../images/icon/menu.png" class="menu" onclick="sideMenu(0)" alt="menu">
+			<img src="images/icon/menu.png" class="menu" onclick="sideMenu(0)" alt="menu">
 		</nav>
 		<div class="head-container">
 			<div class="quote">
 				<p>The beautiful thing about learning is that nobody can take it away from you</p>
 				<h5>Discover a boundless world of knowledge on our quiz website. Embrace the beauty of learning, for it is an everlasting treasure only you possess. Start your journey today!</h5>
 				<div class="play">
-					<img src="../images/icon/play.png" alt="play"><span><a href="../QuizCreationapi/index.php" target="_blank">Play Now</a></span>
+					<img src="images/icon/play.png" alt="play"><span><a href="../QuizCreationapi/index.php" target="_blank">Play Now</a></span>
 				</div>
 			</div>
 			<div class="svg-image">
-				<img src="../images/sample4.png" alt="svg">
+				<img src="images/sample4.png" alt="svg">
 			</div>
 		</div>
 		<div class="side-menu" id="side-menu">
-			<div class="close" onclick="sideMenu(1)"><img src="../images/icon/close.png" alt=""></div>
+			<div class="close" onclick="sideMenu(1)"><img src="images/icon/close.png" alt=""></div>
 			<ul>
 			<?php
                     if (isset($_SESSION["id"])) {
                         if ($_SESSION["user_type"] === "dual") {
 
 							?>
-							<a href="../Profile/user_profile.php" class="profile"><?php echo $_SESSION["username"]; ?></a>
+							<a href="Profile/user_profile.php" class="profile"><?php echo $_SESSION["username"]; ?></a>
 						<?php
 						} elseif ($_SESSION["user_type"] === "participant") {
 		
 						?>
-							<a href="../Includes/logout.inc.php" class="profile"><?php echo $_SESSION["username"]; ?></a>
+							<a href="Includes/logout.inc.php" class="profile"><?php echo $_SESSION["username"]; ?></a>
 						<?php
 						} elseif ($_SESSION["user_type"] === "instructor") {
 		
 						?>
-							<a href="../Includes/logout.inc.php" class="profile"><?php echo $_SESSION["username"]; ?></a>
+							<a href="Includes/logout.inc.php" class="profile"><?php echo $_SESSION["username"]; ?></a>
 						<?php
 						}
                     }
 
                     ?>
-                <li><a href="../Interface/Home.php">Home</a></li>
-                <li><a href="../../QuizCreation/quiz.php">Quiz</a></li>
+                <li><a href="Interface/Home.php">Home</a></li>
+                <li><a href="QuizCreation/quiz.php">Quiz</a></li>
                 <li><a href="blog.php">Blog</a></li>
                 <li><a href="#about_section">About us</a></li>
                 <!-- <li><a href="../Interface/contactus.php">Contact Us</a></li> -->
-                <li><a href="../Interface/team.php">Team</a></li>
+                <li><a href="Interface/team.php">Team</a></li>
 
             </ul>
 		</div>
@@ -149,17 +148,17 @@
 	<div class="course">
 		<center>
 			<div class="cbox">
-				<div class="det"><a href="https://www.youtube.com/watch?v=inWWhr5tnEA"><img src="../images/courses/book.png">Cybersecurity</a></div>
-				<div class="det"><a href="https://www.youtube.com/watch?v=ukzFI9rgwfU"><img src="../images/courses/d1.png">Machine Learning</a></div>
-				<div class="det"><a href="https://www.youtube.com/watch?v=mo1lNRKnayA"><img src="../images/courses/paper.png">5G Technology</a></div>
-				<div class="det"><a href="https://www.youtube.com/watch?v=RrTCjp2015M"><img src="../images/courses/d1.png">Biotechnology</a></div>
+				<div class="det"><a href="https://www.youtube.com/watch?v=inWWhr5tnEA"><img src="images/courses/book.png">Cybersecurity</a></div>
+				<div class="det"><a href="https://www.youtube.com/watch?v=ukzFI9rgwfU"><img src="images/courses/d1.png">Machine Learning</a></div>
+				<div class="det"><a href="https://www.youtube.com/watch?v=mo1lNRKnayA"><img src="images/courses/paper.png">5G Technology</a></div>
+				<div class="det"><a href="https://www.youtube.com/watch?v=RrTCjp2015M"><img src="images/courses/d1.png">Biotechnology</a></div>
 			</div>
 		</center>
 		<div class="cbox">
-			<div class="det"><a href="https://www.youtube.com/@freecodecamp/playlists"><img src="../images/courses/computer.png">Courses</a></div>
-			<div class="det"><a href="https://www.youtube.com/watch?v=BBpAmxU_NQo"><img src="../images/courses/data.png">Data Structures</a></div>
-			<div class="det"><a href="https://www.youtube.com/watch?v=htjRUL3neMg"><img src="../images/courses/algo.png">Robotics</a></div>
-			<div class="det det-last"><a href="https://www.youtube.com/watch?v=xowQkxFXTNg"><img src="../images/courses/projects.png">Automation</a></div>
+			<div class="det"><a href="https://www.youtube.com/@freecodecamp/playlists"><img src="images/courses/computer.png">Courses</a></div>
+			<div class="det"><a href="https://www.youtube.com/watch?v=BBpAmxU_NQo"><img src="images/courses/data.png">Data Structures</a></div>
+			<div class="det"><a href="https://www.youtube.com/watch?v=htjRUL3neMg"><img src="images/courses/algo.png">Robotics</a></div>
+			<div class="det det-last"><a href="https://www.youtube.com/watch?v=xowQkxFXTNg"><img src="images/courses/projects.png">Automation</a></div>
 		</div>
 	</div>
 
@@ -171,7 +170,7 @@
 		</center>
 		<div class="about-content">
 			<div class="side-image">
-				<img class="sideImage" src="../images/sample3.png">
+				<img class="sideImage" src="images/sample3.png">
 			</div>
 			<div class="side-text">
 				<h2>What you think about us ?</h2>
@@ -200,13 +199,13 @@
 
 				<p class="rights-text">Copyright © 2023 Created By UWU TEAM, All Rights Reserved.</p>
 				<br>
-				<p><img src="../images/icon/location.png"> Passara Road, Badulla</p><br>
-				<p><img src="../images/icon/phone.png"> 055-222-4446<br>
-				<p><img src="../images/icon/mail.png"> Quizzify2023@gmail.com
+				<p><img src="images/icon/location.png"> Passara Road, Badulla</p><br>
+				<p><img src="images/icon/phone.png"> 055-222-4446<br>
+				<p><img src="images/icon/mail.png"> Quizzify2023@gmail.com
 				<!-- <p><img src="../../images/icon/mail.png">&nbsp; </p> -->
 			</div>
 			<div class="right-col">
-			<img src="../images/QUIZZIFY.png" style="width: 200px;">
+			<img src="images/QUIZZIFY.png" style="width: 200px;">
 				<div class="logo"></div>
 			</div>
 		</div>
@@ -214,7 +213,7 @@
 
 	
 </body>
-	<script type="text/javascript" src="../Chatbot/script.js"></script>
-	<script type="text/javascript" src="Home.js"></script>
+	<script type="text/javascript" src="Chatbot/script.js"></script>
+	<script type="text/javascript" src="Interface/Home.js"></script>
 	<script src="https://code.jquery.com/jquery-3.2.1.js"></script>
 </html>
